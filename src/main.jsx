a37225c9ac808230669b9935add8b2ae6b7e30ae
abrarasif11 <faasif929@gmail.com>
2025-09-01 19:33:02 +0600

    crate-header

--- src/main.jsx
+++ src/main.jsx
@@ -5,22 +5,30 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import AddCoffee from "./Component/AddCoffee/AddCoffee";
 import UpdateCoffee from "./Component/UpdateCoffee/UpdateCoffee";
+import Main from "./Component/Layout/Main";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
-    loader : () => fetch('http://localhost:5000/coffee')
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+        loader: () => fetch("http://localhost:5000/coffee"),
+      },
+      {
+        path: "/addCoffee",
+        element: <AddCoffee />,
+      },
+      {
+        path: "/updateCoffee/:id",
+        element: <UpdateCoffee />,
+        loader: ({ params }) =>
+          fetch(`http://localhost:5000/coffee/${params.id}`),
+      },
+    ],
   },
-  {
-    path:'/addCoffee',
-    element: <AddCoffee/>
-  },
-  {
-    path:'/updateCoffee/:id',
-    element : <UpdateCoffee/>,
-    loader : ({ params }) => fetch(`http://localhost:5000/coffee/${params.id}`)
-  }
 ]);
 createRoot(document.getElementById("root")).render(
   <StrictMode>
