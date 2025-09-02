dc46f4b97ae295cb9508d1dd79ddcf189bf9a96d
abrarasif11 <faasif929@gmail.com>
2025-09-02 14:43:29 +0600

    deleted-user-from-register-user

--- src/Component/Register/Users/Users.jsx
+++ src/Component/Register/Users/Users.jsx
@@ -2,9 +2,40 @@ import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { MdDelete } from "react-icons/md";
 import { MdEdit } from "react-icons/md";
+import Swal from "sweetalert2";
 const Users = () => {
   const loadedUsers = useLoaderData();
   const [users, setUsers] = useState(loadedUsers);
+  const handleUserDelete = (id) =>
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // delete from DB
+        fetch(`http://localhost:5000/user/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success",
+              });
+              // update state to remove deleted user
+              setUsers(users.filter((user) => user._id !== id));
+            }
+          });
+      }
+    });
+
   return (
     <div>
       Total Users : {users.length}
@@ -29,10 +60,13 @@ const Users = () => {
                   <td>{user.email}</td>
                   <td>{user.createdAt}</td>
                   <td>
-                    <button className="flex items-center justify-center w-12 mb-2 h-12 rounded-lg bg-[#D2B48C] text-white hover:bg-[#c0a070]">
+                    <button className="flex items-center justify-center w-12 mb-2 h-12 rounded-lg bg-[#3C393B] text-white hover:bg-[#c0a070]">
                       <MdEdit className="text-2xl" />
                     </button>
-                    <button className="flex items-center justify-center w-12 h-12 rounded-lg bg-[#D2B48C] text-white hover:bg-[#c0a070]">
+                    <button
+                      onClick={() => handleUserDelete(user._id)}
+                      className="flex items-center justify-center w-12 h-12 rounded-lg bg-[#EA4744] text-white hover:bg-[#c0a070]"
+                    >
                       <MdDelete className="text-2xl" />
                     </button>
                   </td>
